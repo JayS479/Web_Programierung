3021fb9d8eb8712bf116f7aafec191f0165aadf9
JayS479 <144341544+JayS479@users.noreply.github.com>
2023-11-10 14:43:26 +0100

    Push

--- docs/beispiele/detail.js
+++ docs/beispiele/detail.js
@@ -1,87 +1,92 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const userID = urlParams.get("id");
+const productID = urlParams.get("id");
+
 document.addEventListener("DOMContentLoaded", function () {
+const dataContainer = document.getElementById("resultsContainer");
+const cartContainer = document.getElementById("cartContainer");
 
-  const searchResults = document.getElementById("resultsContainer"); //Referenzen zu den HTML-Elementen
+const apiUrl = "https://dummyjson.com/products";
+let url = `https://dummyjson.com/products/${productID}`;
+let urlCart = `https://dummyjson.com/carts/${productID}`;
 
-  const apiUrl = "https://dummyjson.com/products"; //URL der API, von der die Daten abgerufen werden sollen
-  let url = `https://dummyjson.com/users/${userID}/products`;
+fetch(url)
+.then((response) => response.json())
+.then((product) => {
+// Erstelle einen Container für das Produkt
+const productContainer = document.createElement("div");
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        
+// Erstelle ein Bild-Element und setze die Quelle auf das Produktbild
+const imgElement = document.createElement("img");
+imgElement.src = product.thumbnail; // Annahme: Die API gibt ein Bild als Eigenschaft 'image' zurück
+imgElement.alt = "Product Image";
+imgElement.style.width = "30em"; // Setze die Breite des Bildes nach Bedarf
 
-        // Lösche den Container-Inhalt, um vorherige Suchergebnisse zu entfernen
-        dataContainer.innerHTML = "";
+// Erstelle ein Element für den Titel des Produkts
+const titleElement = document.createElement("h2");
+titleElement.textContent = `Titel: ${product.title}`;
 
-        if (data.todos) {
-          data.todos.forEach((todo) => {
-            const todoItem = document.createElement("div");
-            todoItem.textContent = `ID: ${todo.id}, Todo: ${todo.todo}`;
-            dataContainer.appendChild(todoItem);
-          });
-        } else {
-          dataContainer.textContent = "Keine Todos gefunden.";
-        }
+//Füge Description ein
+const descriptionElement = document.createElement("h2");
+descriptionElement.textContent = `Beschreibung: ${product.description}`;
 
-  
+//Füge Description ein
+const priceElement = document.createElement("h2");
+priceElement.textContent = `Preis: ${product.price}`;
 
-  // Funktion zum Ausführen der Suche
-  function performSearch() {
-    const searchTerm = searchInput.value.toLowerCase().trim(); // Hole den Suchbegriff und trimme ihn
-    const filteredProducts = products.filter((product) =>
-      product.title.toLowerCase().includes(searchTerm)
-    );
-    displaySearchResults(filteredProducts); // Zeige nur gefilterte Produkte an
-  }
-  // Event-Listener für den Suchbutton
+//Füge Description ein
+const brandElement = document.createElement("h2");
+brandElement.textContent = `Marke: ${product.brand}`;
 
-  searchButton.addEventListener("click", performSearch);
-  // Event-Listener für die Eingabe im Suchfeld
-  // searchInput.addEventListener("input", performSearch);
-  // Funktion zum Navigieren zur Detailseite
-  function navigateToDetailPage(productId) {
-    window.location.href = `detail.html?id=${productId}`;
-  }
-  // Event-Listener für das Klicken auf Suchergebnisse
-  searchResults.addEventListener("click", function (event) {
-    const clickedItem = event.target.closest(".search-result"); // Das geklickte Suchergebnis finden
-    if (clickedItem) {
-      const productId = clickedItem.getAttribute("data-product-id"); // Produkt-ID aus dem Datenattribut lesen
-      navigateToDetailPage(productId); // Navigiere zur Detailseite mit der ausgewählten Produkt-ID
-    }
-  });
-  function displaySearchResults(productsToDisplay) {
-    searchResults.innerHTML = "";
-    if (!Array.isArray(productsToDisplay) || productsToDisplay.length === 0) {
-      searchResults.innerHTML = "Keine Ergebnisse gefunden.";
-      return;
-    }
-    const resultList = document.createElement("ul");
-    productsToDisplay.forEach((product) => {
-      const listItem = document.createElement("li");
-      listItem.classList.add("search-result"); // CSS-Klasse für das Suchergebnis hinzufügen
-      // Erstelle ein <img>-Element für das Produktbild
-      const productImage = document.createElement("img");
-      productImage.src = product.thumbnail; // Bild-URL aus DummyJson
-      // Erstelle ein <span>-Element für den Produktnamen
-      const productName = document.createElement("span");
-      productName.textContent = product.title;
-      listItem.setAttribute("data-product-id", product.id); // Produkt-ID als Datenattribut hinzufügen
-      listItem.appendChild(productImage); // Füge das Bild zum Listenelement hinzu
-      listItem.appendChild(productName); // Füge den Produktnamen zum Listenelement hinzu
-      resultList.appendChild(listItem); // Füge das Listenelement zur Ergebnisliste hinzu
-    });
-    searchResults.appendChild(resultList); // Füge die Ergebnisliste zu den Suchergebnissen hinzu
-  }
+// Füge das Bild-Element und den Titel dem Produktcontainer hinzu
+productContainer.appendChild(imgElement);
+productContainer.appendChild(titleElement);
+productContainer.appendChild(descriptionElement);
+productContainer.appendChild(priceElement);
+productContainer.appendChild(brandElement);
+
+
+// Füge den Produktcontainer dem Hauptcontainer auf der Seite hinzu
+dataContainer.appendChild(productContainer);
+})
+.catch((error) => {
+console.error("Error fetching product data:", error);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleMenuButton = document.getElementById("toggleMenu");
-  const menuLinks = document.querySelector(".menu-links");
-  toggleMenuButton.addEventListener("click", function () {
-    menuLinks.classList.toggle("active");
-  });
+
+fetch(urlCart)
+.then((response) => response.json())
+.then((cart) => {
+// Erstelle einen Container für das Produkt
+const shopContainer = document.createElement("div");
+
+
+
+
+// Erstelle ein Element für den Titel des Produkts
+const titleElement = document.createElement("h2");
+titleElement.textContent = `Titel: ${cart.products[0].title}`;
+
+
+
+//Füge Description ein
+const priceElement = document.createElement("h2");
+priceElement.textContent = `Preis: ${cart.products[0].price}`;
+
+
+
+// Füge das Bild-Element und den Titel dem Produktcontainer hinzu
+
+shopContainer.appendChild(titleElement);
+
+shopContainer.appendChild(priceElement);
+
+
+
+// Füge den Produktcontainer dem Hauptcontainer auf der Seite hinzu
+cartContainer.appendChild(shopContainer);
+})
+.catch((error) => {
+console.error("Error fetching product data:", error);
+});
 });
