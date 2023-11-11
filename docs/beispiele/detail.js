39c8d10623daeb69df2c32a3befe495ca5ec72a0
JayS479 <144341544+JayS479@users.noreply.github.com>
2023-11-11 14:52:33 +0100

    Update

--- docs/beispiele/detail.js
+++ docs/beispiele/detail.js
@@ -3,7 +3,8 @@ const urlParams = new URLSearchParams(queryString);
 const productID = urlParams.get("id");
 
 document.addEventListener("DOMContentLoaded", function () {
-const dataContainer = document.getElementById("resultsContainer");
+const dataContainer = document.getElementById("detailContainer");
+const imgContainer = document.getElementById("bildContainer");
 const cartContainer = document.getElementById("cartContainer");
 
 const apiUrl = "https://dummyjson.com/products";
@@ -15,6 +16,8 @@ fetch(url)
 .then((product) => {
 // Erstelle einen Container für das Produkt
 const productContainer = document.createElement("div");
+const picContainer = document.createElement("div");
+productContainer.classList.add("detail-result"); // CSS-Klasse für das Suchergebnis hinzufügen
 
 // Erstelle ein Bild-Element und setze die Quelle auf das Produktbild
 const imgElement = document.createElement("img");
@@ -39,7 +42,7 @@ const brandElement = document.createElement("h2");
 brandElement.textContent = `Marke: ${product.brand}`;
 
 // Füge das Bild-Element und den Titel dem Produktcontainer hinzu
-productContainer.appendChild(imgElement);
+picContainer.appendChild(imgElement);
 productContainer.appendChild(titleElement);
 productContainer.appendChild(descriptionElement);
 productContainer.appendChild(priceElement);
@@ -48,6 +51,8 @@ productContainer.appendChild(brandElement);
 
 // Füge den Produktcontainer dem Hauptcontainer auf der Seite hinzu
 dataContainer.appendChild(productContainer);
+imgContainer.appendChild(picContainer);
+
 })
 .catch((error) => {
 console.error("Error fetching product data:", error);
@@ -57,34 +62,37 @@ console.error("Error fetching product data:", error);
 fetch(urlCart)
 .then((response) => response.json())
 .then((cart) => {
-// Erstelle einen Container für das Produkt
-const shopContainer = document.createElement("div");
+// Erstelle einen Container für den Warenkorb
+
+
+for (let i = 0; i < cart.products.length; i++){
+    const shopContainer = document.createElement("div");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = cart.products[i].thumbnail;
+    imgElement.alt = "Product Image";
+    imgElement.style.width = "5em";
+
+    // Erstelle ein Element für den Titel des i-ten Produkts
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = `Titel: ${cart.products[i].title}`;
+    // Erstelle ein Element für den Preis des i-ten Produkts
+    const priceElement = document.createElement("h2");
+    priceElement.textContent = `Preis: ${cart.products[i].price}`;
+    // Erstelle ein Element für die Menge des i-ten Produkts
+    const quantityElement = document.createElement("h2");
+    quantityElement.textContent = `Anzahl: ${cart.products[i].quantity}`;
 
 
 
+    shopContainer.appendChild(imgElement);
+    shopContainer.appendChild(titleElement);
+    shopContainer.appendChild(priceElement);
+    shopContainer.appendChild(quantityElement);
 
-// Erstelle ein Element für den Titel des Produkts
-const titleElement = document.createElement("h2");
-titleElement.textContent = `Titel: ${cart.products[0].title}`;
-
-
-
-//Füge Description ein
-const priceElement = document.createElement("h2");
-priceElement.textContent = `Preis: ${cart.products[0].price}`;
-
-
-
-// Füge das Bild-Element und den Titel dem Produktcontainer hinzu
-
-shopContainer.appendChild(titleElement);
-
-shopContainer.appendChild(priceElement);
-
-
-
-// Füge den Produktcontainer dem Hauptcontainer auf der Seite hinzu
-cartContainer.appendChild(shopContainer);
+    // Füge den Produktcontainer dem Hauptcontainer auf der Seite hinzu
+    cartContainer.appendChild(shopContainer);
+}
 })
 .catch((error) => {
 console.error("Error fetching product data:", error);
